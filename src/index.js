5b55d41ba15360aee03d64fadf2db4091cd87407
Ayush Bansal <ayu023ban@gmail.com>
2020-12-22 10:46:22 +0530

    Add material ui and react-bootstrap for better ui handling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/index';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
